b0c97f54dc9ee5d9cfb86919edc3f9966916868f
Tyler Kindy <tylerkindy@gmail.com>
2022-12-03 22:21:26 -0500

    Finish part 2

--- src/day01.clj
+++ src/day01.clj
@@ -20,6 +20,16 @@
 (defn end-floor [instructions]
   (reduce follow-instruction 0 instructions))
 
+(defn first-basement [instructions]
+  (->> instructions 
+       (reductions (fn [[i floor] inst] 
+                     [(inc i) (follow-instruction floor inst)]) 
+                   [0 0])
+       (filter (fn [[_ floor]] (< floor 0)))
+       first
+       first))
+
 (defn -main []
   (let [instructions (read-input)]
-    (println "Part 1:" (end-floor instructions))))
+    (println "Part 1:" (end-floor instructions))
+    (println "Part 2:" (first-basement instructions))))
