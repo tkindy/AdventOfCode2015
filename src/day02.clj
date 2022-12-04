30518abbafc4a692c647170ffde1cf717f01d526
Tyler Kindy <tylerkindy@gmail.com>
2022-12-04 14:51:12 -0500

    Implement parsing

--- src/day02.clj
+++ src/day02.clj
@@ -1,7 +1,12 @@
-(ns day02)
+(ns day02
+  (:require [clojure.string :as str]))
+
+(defn parse-line [line]
+  (->> (str/split line #"x")
+       (mapv parse-long)))
 
 (defn parse-input [input]
-  (throw (RuntimeException. "Not yet implemented")))
+  (map parse-line (str/split-lines (str/trim input))))
 
 (defn read-input []
   (parse-input (slurp "inputs/day02.txt")))
