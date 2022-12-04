02cf2e68b53d61a922afc0ffd49123f4de249eae
Tyler Kindy <tylerkindy@gmail.com>
2022-12-03 22:13:09 -0500

    Do part 1

--- src/day01.clj
+++ src/day01.clj
@@ -1,13 +1,24 @@
-(ns day01)
+(ns day01
+  (:require [clojure.string :as str]))
+
+(defn parse-instruction [instruction]
+  (case instruction
+    \( :up
+    \) :down))
 
 (defn parse-input [input]
-  (throw (RuntimeException. "Not yet implemented")))
+  (map parse-instruction (str/trim input)))
 
 (defn read-input []
   (parse-input (slurp "inputs/day01.txt")))
 
+(defn follow-instruction [floor instruction]
+  (case instruction
+    :up   (inc floor)
+    :down (dec floor)))
+
 (defn end-floor [instructions]
-  (throw (RuntimeException. "Not yet implemented")))
+  (reduce follow-instruction 0 instructions))
 
 (defn -main []
   (let [instructions (read-input)]
